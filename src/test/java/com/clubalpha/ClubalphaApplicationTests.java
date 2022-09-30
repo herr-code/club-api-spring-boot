e118e2e45cffa8e8fdfed9d9a21d30d57db7ec97
herr-code <herrwebj@gmail.com>
2022-09-29 21:38:51 -0500

    Configuracion inicial

---
+++ src/test/java/com/clubalpha/ClubalphaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.clubalpha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubalphaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
